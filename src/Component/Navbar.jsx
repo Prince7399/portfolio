7149bd72869d8403f3441a4827bad31fe253223f
Prince7399 <princetalaviya2000@gmail.com>
2025-09-10 09:55:26 +0530

    Remove unused code

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -7,29 +7,23 @@ const Navbar = () => {
             <div className="container-fluid top-nav">
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <div class="container-fluid">
-
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav text-white ms-auto">
                                 <li class="nav-item nav-item-list">
-
                                     <NavLink className="nav-link nav-color" to="/"><a class="nav-link nav-color my-active" aria-current="page">Home</a></NavLink>
                                 </li>
                                 <li class="nav-item nav-item-list">
-
                                     <NavLink className="nav-link nav-color" to="/about"><a class="nav-link nav-color my-active">About</a></NavLink>
                                 </li>
                                 <li class="nav-item nav-item-list">
-
                                     <NavLink className="nav-link nav-color" to="/project"><a class="nav-link nav-color my-active">Projects</a></NavLink>
                                 </li>
                                 <li class="nav-item nav-item-list">
-
                                     <NavLink className="nav-link nav-color" to="/contact"><a class="nav-link nav-color my-active">Contact</a></NavLink>
                                 </li>
-
                             </ul>
                         </div>
                     </div>
--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,6 +1,3 @@
-import React from 'react'
-import restaurent from "./../Assets/Images/restaurent_project.png"
-import portfolio from "./../Assets/Images/portfolio_project.png"
 import { project_data } from "./../Assets/API/Project_api";
 
 const Project = () => {
@@ -13,60 +10,6 @@ const Project = () => {
                         <h1>Project Work</h1>
                     </div>
                     <div class="row row-cols-1 row-cols-md-2">
-                        {/* <div class="col-12 col-md-5 mx-auto">
-                            <div class="card h-100 tab-bg" data-aos="zoom-out-right" data-aos-duration="1500">
-                                <div className='p-4'>
-                                    <img src={restaurent} class="card-img-top proj-img" alt="..." />
-                                </div>
-
-                                <div class="card-body">
-                                    <h2 class="card-title text-center theme-color">Restaurent Website</h2>
-                                    <h3 class="card-title text-center theme-color">React Based Restaurent Website</h3>
-                                    <h3 class="card-title mt-3 text-white">Key Features</h3>
-                                    <ul className='text-white'>
-                                        <li>Incorporated all the key features of an e-commerce website in a dynamic manner.</li>
-                                        <li>Handled all composition, color, illustration, typography, and branding.</li>
-                                        <li>Followed best code practices while developing dynamic and reusable code.</li>
-                                    </ul>
-                                    <b className='theme-color'><u>Technology Used</u></b>
-                                    <div className="d-flex justify-content-around mt-2">
-                                        <i class="fa-brands fa-html5 tech-proj-icon"></i>
-                                        <i class="fa-brands fa-css3-alt tech-proj-icon"></i>
-                                        <i class="fa-brands fa-square-js tech-proj-icon"></i>
-                                        <i class="fa-brands fa-bootstrap tech-proj-icon"></i>
-                                        <i class="fa-brands fa-react tech-proj-icon"></i>
-                                        <i class="fa-brands fa-git-alt tech-proj-icon"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-12 col-md-5 mx-auto">
-                            <div class="card h-100 tab-bg" data-aos="zoom-out-left" data-aos-duration="1500">
-                                <div className='p-4'>
-                                    <img src={portfolio} class="card-img-top proj-img" alt="..." />
-                                </div>
-                                <div class="card-body">
-                                    <h2 class="card-title text-center theme-color">Portfolio Website</h2>
-                                    <h3 class="card-title text-center theme-color">React Based Portfolio Website</h3>
-                                    <h3 class="card-title mt-3 text-white">Key Features</h3>
-                                    <ul className='text-white'>
-                                        <li>Incorporated all the key features of an e-commerce website in a dynamic manner.</li>
-                                        <li>Created all the components from scratch using react and java script.</li>
-                                        <li>Followed best code practices while developing dynamic and reusable code.</li>
-                                    </ul>
-                                    <b className='theme-color'><u>Technology Used</u></b>
-                                    <div className="d-flex justify-content-around mt-2">
-                                        <i class="fa-brands fa-html5 tech-proj-icon"></i>
-                                        <i class="fa-brands fa-css3-alt tech-proj-icon"></i>
-                                        <i class="fa-brands fa-square-js tech-proj-icon"></i>
-                                        <i class="fa-brands fa-bootstrap tech-proj-icon"></i>
-                                        <i class="fa-brands fa-react tech-proj-icon"></i>
-                                        <i class="fa-brands fa-git-alt tech-proj-icon"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> */}
                         {
                             project_data.map((data, ind) => {
                                 return (
@@ -82,9 +25,6 @@ const Project = () => {
                                                     <h3 class="card-title text-center theme-color">React Based Portfolio Website</h3>
                                                     <h3 class="card-title mt-3 text-white">Key Features</h3>
                                                     <ul className='text-white'>
-                                                        {/* <li>Incorporated all the key features of an e-commerce website in a dynamic manner.</li>
-                                                        <li>Handled all composition, color, illustration, typography, and branding.</li>
-                                                        <li>Followed best code practices while developing dynamic and reusable code.</li> */}
                                                         {
                                                             data.key.map((liVal, ind) => {
                                                                 return (
@@ -97,12 +37,6 @@ const Project = () => {
                                                     </ul>
                                                     <b className='theme-color'><u>Technology Used</u></b>
                                                     <div className="d-flex justify-content-around mt-2">
-                                                        {/* <i class="fa-brands fa-html5 tech-proj-icon"></i>
-                                                        <i class="fa-brands fa-css3-alt tech-proj-icon"></i>
-                                                        <i class="fa-brands fa-square-js tech-proj-icon"></i>
-                                                        <i class="fa-brands fa-bootstrap tech-proj-icon"></i>
-                                                        <i class="fa-brands fa-react tech-proj-icon"></i>
-                                                        <i class="fa-brands fa-git-alt tech-proj-icon"></i> */}
                                                         {
                                                             data.technology.map((icon, ind) => {
                                                                 return (
@@ -121,37 +55,6 @@ const Project = () => {
                                 )
                             })
                         }
-
-                        {/* <div class="col-12 col-md-5 mx-auto">
-                            <div class="card h-100 tab-bg" data-aos="zoom-out-right" data-aos-duration="1500">
-                                <div className='p-4'>
-                                    <img src={restaurent} class="card-img-top proj-img" alt="..." />
-                                </div>
-
-                                <div class="card-body">
-                                    <h2 class="card-title text-center theme-color">Restaurent Website</h2>
-                                    <h3 class="card-title text-center theme-color">React Based Restaurent Website</h3>
-                                    <h3 class="card-title mt-3 text-white">Key Features</h3>
-                                    <ul className='text-white'>
-                                        <li>Incorporated all the key features of an e-commerce website in a dynamic manner.</li>
-                                        <li>Handled all composition, color, illustration, typography, and branding.</li>
-                                        <li>Followed best code practices while developing dynamic and reusable code.</li>
-                                    </ul>
-                                    <b className='theme-color'><u>Technology Used</u></b>
-                                    <div className="d-flex justify-content-around mt-2">
-                                        <i class="fa-brands fa-html5 tech-proj-icon"></i>
-                                        <i class="fa-brands fa-css3-alt tech-proj-icon"></i>
-                                        <i class="fa-brands fa-square-js tech-proj-icon"></i>
-                                        <i class="fa-brands fa-bootstrap tech-proj-icon"></i>
-                                        <i class="fa-brands fa-react tech-proj-icon"></i>
-                                        <i class="fa-brands fa-git-alt tech-proj-icon"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> */}
-
-
-
                     </div>
                 </div>
             </section>
